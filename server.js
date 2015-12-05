4c7637bef2da65a744795e3e85719f74f508351a
Jon Tejada <tejjon@gmail.com>
2015-12-04 16:38:45 -0800

    refactored for all urls

--- server.js
+++ server.js
@@ -1,22 +1,37 @@
 var http = require('http');
 var fs   = require('fs');
-var routes = require('./routes');
+//var routes = require('./routes');
 var PORT = 8000;
+//var url = require('url');
 
 function handleRequest(req, res) {
-	console.log(req.url);
- 	if (routes[req.url]) {
-		routes[req.url](req, res);
-	} else {
-		res.statusCode = 404;
-		res.setHeader("Content-Type","text/html");
-		res.write("<link rel='stylesheet' type='text/css' href='style.css'><h1>404! NOT FOUND</h1><img src='https://http.cat/404'><br><a href='index.html'>Home</a><br><a href='about.html'>About</a>");
-    	res.end();
-    }
+	var extension = req.url.split('.').pop();
+
+	fs.readFile('.' + req.url, function (err, data) {
+		if (err) {
+			res.setHeader("Content-Type","text/html");
+			res.statusCode = 404;
+			res.write("<link rel='stylesheet' type='text/css' href='style.css'><h1>404! NOT FOUND</h1><img src='https://http.cat/404'><br><a href='index.html'>Home</a><br><a href='about.html'>About</a>");
+	    	res.end();
+	    	return;
+		}
+		res.setHeader("Content-Type","text/" + extension);
+		res.write(data);
+		res.end();
+	});
+
+ // 	if (routes[req.url]) {
+	// 	routes[req.url](req, res);
+	// } else {
+	// 	res.statusCode = 404;
+	// 	res.setHeader("Content-Type","text/html");
+	// 	res.write("<link rel='stylesheet' type='text/css' href='style.css'><h1>404! NOT FOUND</h1><img src='https://http.cat/404'><br><a href='index.html'>Home</a><br><a href='about.html'>About</a>");
+ //    	res.end();
+ //    }
 }
 
 var server = http.createServer(handleRequest);
 
 server.listen(PORT, function() {
-	console.log("I'm listening on port" + PORT);
+	console.log("I'm listening on port " + PORT);
 });
